1906232c7e9f3d1f2e24d90161a904cd96dbdee7
Nandu-Chopade <nanduchopade1998@gmail.com>
2023-09-16 13:00:48 +0530

    fromating code and removing hard sql queries

--- src/main/java/com/ecommerce/repository/CustomerDetailsRepository.java
+++ src/main/java/com/ecommerce/repository/CustomerDetailsRepository.java
@@ -1,6 +1,6 @@
 package com.ecommerce.repository;
 
-
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,107 +11,55 @@ import org.springframework.stereotype.Repository;
 import com.ecommerce.model.CustomerDetails;
 
 @Repository
-public class CustomerDetailsRepository{
-   
+public class CustomerDetailsRepository {
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
 	// save
 	public int save(CustomerDetails customerDetails) {
-		
-		int insertedRow = jdbcTemplate.update("insert into customer_details"
-				+ "(firstName, lastName, username, password, mobile, email,address) "
-				+ "value(?,?,?,?,?,?,?)", 
-				customerDetails.getFirstName(),
-				customerDetails.getLastName(),
-				customerDetails.getUsername(),
-				customerDetails.getPassword(),
-				customerDetails.getMobile(),
-				customerDetails.getEmail(),
-				customerDetails.getAddress());
-		
-		return insertedRow;
+		String saveQuery = "insert into customer_details"
+				+ "(firstName, lastName, username, password, mobile, email,address) " + "value(?,?,?,?,?,?,?)";
+		return jdbcTemplate.update(saveQuery, customerDetails.getFirstName(), customerDetails.getLastName(),
+				customerDetails.getUsername(), customerDetails.getPassword(), customerDetails.getMobile(),
+				customerDetails.getEmail(), customerDetails.getAddress());
+
 	}
-	
+
 	@SuppressWarnings("deprecation")
 	public List<CustomerDetails> findById(int customerId) {
-		
-		
-		     return jdbcTemplate.query(
-				"select * from customer_details where customerId = ?",
-				new Object[] { customerId },
-				(rs ,rowNum) -> 
-						
-						new CustomerDetails(
-						
-								rs.getInt("customerId"),
-								rs.getString("firstName"),
-								rs.getString("lastName"),
-								rs.getString("username"),
-								rs.getString("password"),
-								rs.getLong("mobile"),
-								rs.getString("email"),
-								rs.getString("address"),
-								rs.getInt("billId")
-								
-						)
-				
-				);
-		    
-		    
+		String findByIdQuery = "select * from customer_details where customerId = ?";
+		return jdbcTemplate.query(findByIdQuery, new Object[] { customerId },
+				(rs, rowNum) -> new CustomerDetails(rs.getInt("customerId"), rs.getString("firstName"),
+						rs.getString("lastName"), rs.getString("username"), rs.getString("password"),
+						rs.getLong("mobile"), rs.getString("email"), rs.getString("address"), rs.getInt("billId")));
 
 	}
-	
-	
-	// findAll
-	 public List<CustomerDetails> findAll(){
-		
-		return  jdbcTemplate.query(
-				"select * from customer_details", 
-				(rs , rowNum) -> 
-						
-						new CustomerDetails(
-						
-								rs.getInt("customerId"),
-								rs.getString("firstName"),
-								rs.getString("lastName"),
-								rs.getString("username"),
-								rs.getString("password"),
-								rs.getLong("mobile"),
-								rs.getString("email"),
-								rs.getString("address"),
-								rs.getInt("billId")
-								
-						)
-				
-				);
-		 
-		
 
-	 }
-	 
-	 //update
-	 public int update(CustomerDetails customerDetails) {
-		   return jdbcTemplate.update(
-	                "update customer_details set firstName = ? , lastName = ? , "
-	                + " email = ?, mobile = ?, address = ?  where customerId = ?",
-	              
-	                customerDetails.getFirstName(),
-	                customerDetails.getLastName(),
-	                customerDetails.getEmail(),
-	                customerDetails.getMobile(),
-	                customerDetails.getAddress(),
-	                customerDetails.getCustomerId()
-	                ); 
-		
-	 }
-	 
-	 
-	 
-	 // deleteById
-	  public int deleteById(int customerId) {
-		  return jdbcTemplate.update("delete customer_details where customerId = ? ", customerId );
-	  }
-	 
-	
+	// findAll
+	public List<CustomerDetails> findAll() {
+		String findListQuery = "select * from customer_details";
+		return jdbcTemplate.query(findListQuery,
+				(rs, rowNum) -> new CustomerDetails(rs.getInt("customerId"), rs.getString("firstName"),
+						rs.getString("lastName"), rs.getString("username"), rs.getString("password"),
+						rs.getLong("mobile"), rs.getString("email"), rs.getString("address"), rs.getInt("billId")));
+
+	}
+
+	// update
+	public int update(CustomerDetails customerDetails) {
+		String updateQuery = "update customer_details set firstName = ? , lastName = ? , "
+				+ "email = ?, mobile = ?, address = ?  where customerId = ?";
+		return jdbcTemplate.update(updateQuery, customerDetails.getFirstName(), customerDetails.getLastName(),
+				customerDetails.getEmail(), customerDetails.getMobile(), customerDetails.getAddress(),
+				customerDetails.getCustomerId());
+
+	}
+
+	// deleteById
+	public int deleteById(int customerId) throws SQLException {
+		String deleteQuery = "delete customer_details where customerId = ? ";
+		return jdbcTemplate.update(deleteQuery, customerId);
+	}
+
 }
