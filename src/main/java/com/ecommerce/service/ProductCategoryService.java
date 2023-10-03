336da5753fa540ae157c1ea633cec6fc2a75a582
NalineeShinde <shindenalinee96@gmail.com>
2023-10-03 14:54:17 +0530

    Added proper comments to category API

--- src/main/java/com/ecommerce/service/ProductCategoryService.java
+++ src/main/java/com/ecommerce/service/ProductCategoryService.java
@@ -9,14 +9,43 @@ import com.ecommerce.model.ProductCategory;
  */
 public interface ProductCategoryService {
 
+	/**
+	 * This method is to add new Product Category
+	 * 
+	 * @param productCategory
+	 * @return Integer
+	 */
 	Integer addProductCategory(ProductCategory productCategory);
 
+	/**
+	 * This method is to find All existing Product Categories.
+	 * 
+	 * @return List<ProductCategory>
+	 */
 	List<ProductCategory> findAllProductCategories();
 
+	/**
+	 * This method is to update existing Product Category.
+	 * 
+	 * @param productCategory
+	 * @param categoryId
+	 * @return Integer
+	 */
 	Integer updateProductCategory(ProductCategory productCategory, Integer categoryId);
 
+	/**
+	 * This method is to find Product Category By Id.
+	 * 
+	 * @param categoryId
+	 * @return ProductCategory
+	 */
 	ProductCategory findProductCategoryById(Integer categoryId);
 
+	/**
+	 * This method is to delete Existing Product Category By Id.
+	 * 
+	 * @param categoryId
+	 */
 	void deleteProductCategoryById(Integer categoryId);
 
 }
